76030d993a6d510bb51ced49da923c47324ade83
Oliver Eaton <eat_fred@protonmail.com>
2023-02-25 23:56:41 +0200

    begin package build

--- pkg.R
+++ pkg.R
@@ -1,10 +1,11 @@
 library(usethis)
 library(here)
 
-create_package(here())
+# create_package(here())
 
 use_mit_license("Oliver Eaton")
-use_package("mapBliss", "purrr")
+use_package("purrr", "Imports")
+use_dev_package("mapBliss", "Imports", "https://github.com/benyamindsmith/mapBliss.git")
 use_readme_md()
 use_news_md()
 use_test("my-test")
